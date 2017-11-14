32a606fd03a191d1f12a0150add3576bf1febf17
Kevin Cherrington <kevcherrington@gmail.com>
2017-11-14 09:45:11 -0700

    Added a frontend Using Angular-cli I have added a front end to the spring server and re-worked the POM files so that there is a parent pom that will build both the back end and the front end.

--- backend/src/main/java/com/kevcherrinstg/javatechchallenge/controllers/HowdyController.java
+++ backend/src/main/java/com/kevcherrinstg/javatechchallenge/controllers/HowdyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HowdyController {
 
-    @RequestMapping("/")
+    @RequestMapping("/howdy")
     public String index() {
         return "Howdy Y'all!";
     }
